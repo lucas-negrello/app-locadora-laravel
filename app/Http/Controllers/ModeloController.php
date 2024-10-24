c2da357a5b674aae0783d9e7dcbfe3baa4fef43e
negrello <lucassnegrello@gmail.com>
2024-10-23 23:40:49 -0300

    feat/LOC7 - CRUD for Modelo have been made. Updated model Modelo and controller ModeloController. Tested endpoints

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ModeloController extends Controller
 {
+
+    public function __construct(Modelo $modelo){
+        $this->modelo = $modelo;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $modelo = $this->modelo->all();
+        return response()->json($modelo);
     }
 
 
@@ -21,30 +27,83 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->rules());
+        $imagem = $request->file('imagem');
+        $imagem_urn = $imagem->store('imagens/modelos', 'public');
+        $modelo = $this->modelo->create([
+            'marca_id' => $request->get('marca_id'),
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs,
+        ]);
+        return response()->json($modelo, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+        if($modelo === null){
+            return response()->json(['erro' => 'Nenhum registro encontrado'], 404);
+        }
+        return response()->json($modelo);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+        if($modelo === null){
+            return response()->json(['erro' => 'Nenhum registro encontrado'], 404);
+        }
+        if($request->method() === 'PATCH'){
+            $dinamicRules = array();
+            foreach ($modelo->rules() as $input => $rule){
+                if(array_key_exists($input, $request->all())){
+                    $dinamicRules[$input] = $rule;
+                }
+            }
+            $request->validate($dinamicRules);
+        }else{
+            $request->validate($modelo->rules());
+        }
+
+        if($request->file('imagem')){
+            Storage::disk('public')->delete($modelo->imagem);
+        }
+
+        $imagem = $request->file('imagem');
+        $imagem_urn = $imagem->store('imagens/modelos', 'public');
+        $modelo->update([
+            'marca_id' => $request->get('marca_id'),
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs,
+        ]);
+        return response()->json($modelo);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+        if($modelo === null){
+            return response()->json(['erro' => 'Nenhum registro encontrado'], 404);
+        }
+        Storage::disk('public')->delete($modelo->imagem);
+        $modelo->delete();
+        return response()->json($modelo);
     }
 }
